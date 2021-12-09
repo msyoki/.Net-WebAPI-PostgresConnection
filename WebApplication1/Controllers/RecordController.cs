8f850b97b1e262b405459ccad4b8285eb1e07b9e
msyoki <msyokimutua@gmail.com>
2021-12-09 10:36:51 +0300

    initial commit

---
+++ WebApplication1/Controllers/RecordController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public RecordController(IConfiguration configurations)
+        {
+            _config = configurations;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select RecordId as ""RecordId"",
+                        RecordName as ""RecordName"" ,
+                        Country as ""Country"",
+                        RecordDate as ""RecordDate"" ,
+                        DepartmentName as ""DepartmentName"" ,
+                        TransactionType as ""TransactionType"" ,
+                        RecordStatus as ""RecordStatus"" ,
+                        ClientName as ""ClientName"" ,
+                        ClientEmail as ""ClientEmail"" 
+                    from Record
+            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _config.GetConnectionString("RecordAppcon");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection mycon = new NpgsqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using(NpgsqlCommand mycommand=new NpgsqlCommand(query, mycon))
+                {
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult(table);
+
+        }
+    }
+}
---
+++ WebApplication1/Models/Record.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Record
+    {
+        public string RecordId { get; set; }
+        public string RecordName { get; set; }
+        public string Country { get; set; }
+        public string RecordDate { get; set; }
+        public string DepartmentName { get; set; }
+        public string TransactionType { get; set; }
+        public string RecordStatus { get; set; }
+        public string ClientName { get; set; }
+        public string ClientEmail { get; set; }
+        
+    }
+}
+
